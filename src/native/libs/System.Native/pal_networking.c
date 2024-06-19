ffed8fb99049ebe322508dbeb39caa8fff180bab
Dawid Jurczak <dawid_jurek@vp.pl>
2024-06-19 16:10:22 +0200

    [RISC-V] Do not pass null optionValue to getsockopt(_, SOL_TCP, TCP_KEEPIDLE, _, [0])

--- src/native/libs/System.Native/pal_networking.c
+++ src/native/libs/System.Native/pal_networking.c
@@ -2086,6 +2086,16 @@ int32_t SystemNative_GetSockOpt(
     {
         return Error_EFAULT;
     }
+    #if defined(TARGET_RISCV64)
+    uint8_t nonEmptyOptionValueBuffer;
+        if (socketOptionLevel == SocketOptionLevel_SOL_TCP && socketOptionName == SocketOptionName_SO_TCP_KEEPALIVE_TIME && optionValue == NULL
+                && *optionLen == 0)
+        {
+            // It's workaround for Qemu bug: https://gitlab.com/qemu-project/qemu/-/issues/2390.
+            printf("Qemu bug\n");
+            optionValue = &nonEmptyOptionValueBuffer;
+        }
+    #endif
 
     int fd = ToFileDescriptor(socket);
 
